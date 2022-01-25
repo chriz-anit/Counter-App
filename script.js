aec5e0cc387472ca4dc10ecd0fd81b47e0a89a85
chriz-anit <79055929+chriz-anit@users.noreply.github.com>
2022-01-25 20:29:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var data=78;
+document.getElementById("root").innerText=data;
+function decrement(){
+    data=data-1;
+    document.getElementById("root").innerText=data;
+}
+function increment(){
+    data=data+1;
+     document.getElementById("root").innerText=data;
+}
